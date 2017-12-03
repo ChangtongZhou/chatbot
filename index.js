ac1446eb35c1f841151173c91028de3123cd8b4d
Changtong Zhou <changtongzhou@gmail.com>
2017-12-02 20:29:06 -0800

    organized index.js

--- index.js
+++ index.js
@@ -39,6 +39,7 @@ app.use(bodyParser.json())
 
 /* ===== ROUTES =============================================================== */
 import webhook from './routes/webhook';
+app.use('/webhook', webhook);
 
 /* 
  * Add webhook verification:
