f59145c89348ea2f3fdb79139dc9f5dc581ca8d0
Changtong Zhou <changtongzhou@gmail.com>
2017-12-02 18:29:35 -0800

    edited tokens

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ app.post('/webhook', (req, res) => {
 app.get('/webhook', (req, res) => {
 
   // Your verify token. Should be a random string.
-  let VERIFY_TOKEN = "Your_Chatbot"
+  let VERIFY_TOKEN = "Your_Fun_Chatbot"
     
   // Parse the query params
   let mode = req.query['hub.mode'];
