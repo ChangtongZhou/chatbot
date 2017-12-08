cf130e0a5c7a7a34843e689669083a61db1d7ec0
Changtong Zhou <changtongzhou@gmail.com>
2017-12-07 23:06:05 -0800

    control msgs

--- index.js
+++ index.js
@@ -457,6 +457,8 @@ function callSendAPI (sender_psid, response) {
     "message": response
   }
 
+  console.log("Sending '" + response + "' to " + sender_psid);
+
   // Send the HTTP request to the Messenger Platform
   request ({
     "uri": "https://graph.facebook.com/v2.6/me/messages",
