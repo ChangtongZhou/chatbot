0e9b459181f723829718ae9a42f1decc1bc57478
Changtong Zhou <changtongzhou@gmail.com>
2017-12-07 23:29:47 -0800

    control msgs

--- index.js
+++ index.js
@@ -434,6 +434,8 @@ function handlePostback(sender_psid, received_postback) {
       console.log ("Got User Info: " + JSON.stringify(userInfo));
       // var userName = JSON.stringify(userInfo.firstName);
       response = { "text": "Hello, Welcome to your to_do_list bot!!" };
+
+      callSendAPI(sender_psid, response);
       // response = {"text": `Hello, "${userInfo.firstName}"! Welcome to your to_do_list bot!!`};
       // PersistentCallSendAPI(sender_psid, response);
     }, function (err) {
